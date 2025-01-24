04d5459ad099f645bf97a53652090829e90681e2
Matus Klasovity <matus.klasovity@goodrequest.com>
2025-01-24 12:17:37 +0100

    WIP: Toggle accessibility

--- GoodSwiftUI-Sample/GoodSwiftUI-Sample/Screens/GRToggleSampleView.swift
+++ GoodSwiftUI-Sample/GoodSwiftUI-Sample/Screens/GRToggleSampleView.swift
@@ -13,56 +13,58 @@ struct GRToggleSampleView: View {
     @State private var isOn: Bool = false
     
     var body: some View {
-        VStack {
-            Group {
-                Toggle(isOn: $isOn) { text("Toggle large") }
-                    .toggleStyle(GRSwitchStyle(appearance: .default, size: .large))
-                
-                Toggle(isOn: $isOn) { text("Toggle small") }
-                    .toggleStyle(GRSwitchStyle(appearance: .default, size: .small))
-                
-                Toggle(isOn: $isOn) { text("Toggle disabled") }
-                    .toggleStyle(GRSwitchStyle(appearance: .default, size: .small))
-                    .disabled(true)
-                
-                Toggle(isOn: $isOn) { text("Check circle large") }
-                    .toggleStyle(GRToggleStyle(appearance: .default, style: .checkCircle, size: .large))
-                
-                Toggle(isOn: $isOn) { text("Check circle small") }
-                    .toggleStyle(GRToggleStyle(appearance: .default, style: .checkCircle, size: .small))
-                
-                Toggle(isOn: $isOn) { text("Check circle disabled") }
-                    .toggleStyle(GRToggleStyle(appearance: .default, style: .checkCircle, size: .small))
-                    .disabled(true)
-                
-                Toggle(isOn: $isOn) { text("Radio box large") }
-                    .toggleStyle(GRToggleStyle(appearance: .default, style: .radio, size: .large))
-                
-                Toggle(isOn: $isOn) { text("Radio box small") }
-                    .toggleStyle(GRToggleStyle(appearance: .default, style: .radio, size: .small))
-                
-                Toggle(isOn: $isOn) { text("Radio box disabled") }
-                    .toggleStyle(GRToggleStyle(appearance: .default, style: .radio, size: .small))
-                    .disabled(true)
-                
-                Toggle(isOn: $isOn) { text("Check Box small") }
-                    .toggleStyle(GRToggleStyle(appearance: .default, style: .checkBox, size: .large))
-                
-                Toggle(isOn: $isOn) { text("Check Box small") }
-                    .toggleStyle(GRToggleStyle(appearance: .default, style: .checkBox, size: .small))
-                
-                Toggle(isOn: $isOn) { text("Check Box small") }
-                    .toggleStyle(GRToggleStyle(appearance: .default, style: .checkBox, size: .small))
-                    .disabled(true)
-                    
+        ScrollView {
+            VStack {
+                Group {
+                    Toggle(isOn: $isOn) { text("Toggle large") }
+                        .toggleStyle(GRSwitchStyle(appearance: .default, size: .large))
+
+                    Toggle(isOn: $isOn) { text("Toggle small") }
+                        .toggleStyle(GRSwitchStyle(appearance: .default, size: .small))
+
+                    Toggle(isOn: $isOn) { text("Toggle disabled") }
+                        .toggleStyle(GRSwitchStyle(appearance: .default, size: .small))
+                        .disabled(true)
+
+                    Toggle(isOn: $isOn) { text("Check circle large") }
+                        .toggleStyle(GRToggleStyle(appearance: .default, style: .checkCircle, size: .large))
+
+                    Toggle(isOn: $isOn) { text("Check circle small") }
+                        .toggleStyle(GRToggleStyle(appearance: .default, style: .checkCircle, size: .small))
+
+                    Toggle(isOn: $isOn) { text("Check circle disabled") }
+                        .toggleStyle(GRToggleStyle(appearance: .default, style: .checkCircle, size: .small))
+                        .disabled(true)
+
+                    Toggle(isOn: $isOn) { text("Radio box large") }
+                        .toggleStyle(GRToggleStyle(appearance: .default, style: .radio, size: .large))
+
+                    Toggle(isOn: $isOn) { text("Radio box small") }
+                        .toggleStyle(GRToggleStyle(appearance: .default, style: .radio, size: .small))
+
+                    Toggle(isOn: $isOn) { text("Radio box disabled") }
+                        .toggleStyle(GRToggleStyle(appearance: .default, style: .radio, size: .small))
+                        .disabled(true)
+
+                    Toggle(isOn: $isOn) { text("Check Box small") }
+                        .toggleStyle(GRToggleStyle(appearance: .default, style: .checkBox, size: .large))
+
+                    Toggle(isOn: $isOn) { text("Check Box small") }
+                        .toggleStyle(GRToggleStyle(appearance: .default, style: .checkBox, size: .small))
+
+                    Toggle(isOn: $isOn) { text("Check Box small") }
+                        .toggleStyle(GRToggleStyle(appearance: .default, style: .checkBox, size: .small))
+                        .disabled(true)
+
+                }
+                .padding(.horizontal, 16)
+                .padding(.vertical, 8)
             }
-            .padding(.horizontal, 16)
-            .padding(.vertical, 8)
+            .frame(maxWidth: .infinity)
+            .background(.gray.opacity(0.2))
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .padding()
         }
-        .frame(maxWidth: .infinity)
-        .background(.gray.opacity(0.2))
-        .clipShape(RoundedRectangle(cornerRadius: 8))
-        .padding()
     }
     
 }
